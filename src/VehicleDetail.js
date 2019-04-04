3789ba8c339c78c81a432547994290dfd511a6b8
Andres <andres.salazar@ceiba.com.co>
2019-04-04 15:07:15 -0500

    concept test

---
+++ src/VehicleDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class VehicleDetail extends Component {
+
+  render() {
+    return <h1>Hello, {this.props.list}</h1>;
+  }
+
+}
+
+export default VehicleDetail
